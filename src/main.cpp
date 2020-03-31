fef221244757729a008e53e9a04ec5eaaa9f0609
ryanvrosch <ryanvrosch@hotmail.com>
2020-03-31 01:11:23 +0000

    travis update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 #include "GPIO.h"
-void setup() {
-  // put your setup code here, to run once:
+
+
+// blink example
+
+GPIO LED1	= { .DIR = OUTPUT,	.PORT = PORT_B, .PIN =  5}; // pin 13  on arduino (LED)
+GPIO LED2;
+
+void setup() 
+{
+  // call init when struct is filled 
+   GPIO_InitPinFromStruct(&LED1);
+
+   // call init with pin/port settings
+   GPIO_InitPin(&LED2, OUTPUT,PORT_B,4); //  pin 12  on arduino (LED)
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+
+void loop() 
+{
+  delay(1000);
+  GPIO_PinWrite(&LED1,HIGH);
+  delay(1000);
+  GPIO_PinWrite(&LED1,LOW);
 }
